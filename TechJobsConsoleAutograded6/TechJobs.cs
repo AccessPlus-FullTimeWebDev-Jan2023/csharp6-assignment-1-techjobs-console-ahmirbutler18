6a501a1b4cd1637b4ef109f6898c51e0965dc37c
Ahmir <ahmirbutler18@gmail.com>
2023-03-19 19:04:30 -0500

    Task one completed, Test on VS not working so trying to use github auto grader

--- TechJobsConsoleAutograded6/TechJobs.cs
+++ TechJobsConsoleAutograded6/TechJobs.cs
@@ -2,8 +2,8 @@
 
 namespace TechJobsConsoleAutograded6
 {
-	public class TechJobs
-	{
+    public class TechJobs
+    {
         public void RunProgram()
         {
             // Create two Dictionary vars to hold info for menu and data
@@ -135,8 +135,22 @@ namespace TechJobsConsoleAutograded6
         // TODO: complete the PrintJobs method.
         public void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("PrintJobs is not implemented yet");
+            //Console.WriteLine("PrintJobs is not implemented yet");
+            if (someJobs.Count == 0)
+            {
+                Console.WriteLine("No results.");
+
+            }
+            foreach (Dictionary<string, string> job in someJobs)
+            {
+                Console.WriteLine("*****");
+           foreach (KeyValuePair<string, string> kvp in job)
+                {
+                    Console.WriteLine(kvp.Key + ": " + kvp.Value);
+                }
+                Console.WriteLine("*****" + Environment.NewLine);
+            }
         }
+
     }
 }
-
